699bb01745e0caa1bcb2f6c2d32c0a88b3b2b610
Pristi Zahara <pristizzz@gmail.com>
2020-04-23 02:18:47 +0700

    enable phalcon debug

--- app/views/index/index.volt
+++ app/views/index/index.volt
@@ -4,4 +4,4 @@
 
 <p>You're now flying with Phalcon. Great things are about to happen!</p>
 
-<p>This page is located at <code>views/index/index.phtml</code></p>
+<p>This page is located at <code>views/index/index.volt</code></p>
--- public/index.php
+++ public/index.php
@@ -5,43 +5,40 @@ use Phalcon\Di\FactoryDefault;
 
 error_reporting(E_ALL);
 
+(new Phalcon\Debug)->listen();
+
 define('BASE_PATH', dirname(__DIR__));
 define('APP_PATH', BASE_PATH . '/app');
 
-try {
-    /**
-     * The FactoryDefault Dependency Injector automatically registers
-     * the services that provide a full stack framework.
-     */
-    $di = new FactoryDefault();
+/**
+ * The FactoryDefault Dependency Injector automatically registers
+ * the services that provide a full stack framework.
+ */
+$di = new FactoryDefault();
 
-    /**
-     * Read services
-     */
-    include APP_PATH . '/config/services.php';
+/**
+ * Read services
+ */
+include APP_PATH . '/config/services.php';
 
-    /**
-     * Handle routes
-     */
-    include APP_PATH . '/config/router.php';
+/**
+ * Handle routes
+ */
+include APP_PATH . '/config/router.php';
 
-    /**
-     * Get config service for use in inline setup below
-     */
-    $config = $di->getConfig();
+/**
+ * Get config service for use in inline setup below
+ */
+$config = $di->getConfig();
 
-    /**
-     * Include Autoloader
-     */
-    include APP_PATH . '/config/loader.php';
+/**
+ * Include Autoloader
+ */
+include APP_PATH . '/config/loader.php';
 
-    /**
-     * Handle the request
-     */
-    $application = new \Phalcon\Mvc\Application($di);
+/**
+ * Handle the request
+ */
+$application = new \Phalcon\Mvc\Application($di);
 
-    echo $application->handle($_SERVER['REQUEST_URI'])->getContent();
-} catch (\Exception $e) {
-    echo $e->getMessage() . '<br>';
-    echo '<pre>' . $e->getTraceAsString() . '</pre>';
-}
+echo $application->handle($_SERVER['REQUEST_URI'])->getContent();
